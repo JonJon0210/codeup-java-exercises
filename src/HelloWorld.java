8b6c1526135fe6724b78a5dff702690b9407ec1c
Jon Cortez <cortez.m.jon@gmail.com>
2021-01-21 14:23:41 -0600

    step 13

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -13,11 +13,31 @@ public class HelloWorld {
 //        System.out.println(x++);
 //        System.out.println(x);
 
-        int x = 5;
-        System.out.println(++x);
-        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
 
 //        String class;
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        int three = (int) "three";
+
+//        int x = 4;
+////        x = x + 5;
+//        x += 5; //refactored shorthand
+
+//        int x = 3;
+//        int y = 4;
+////        y = y * x;
+//        y *= x; //refactored shorthand
+
+//        int x = 10;
+//        int y = 2;
+//////        x = x / y;
+//////        y = y - x;
+//        x /= y; //refactored shorthand
+//        y -= x; //refactored shorthand
 
     }
 }
