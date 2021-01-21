cea853a05d87285d515dcd8ef485d0e7c4477cc5
Jon Cortez <cortez.m.jon@gmail.com>
2021-01-21 14:04:43 -0600

    step 11

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,21 @@ public class HelloWorld {
         //System.out.println("Hello, World!");
         int myFavoriteNumber = 7;
         System.out.println(myFavoriteNumber);
-        String myString = "Codeup";
+        String myString = "3.14159";
         System.out.println(myString);
+        double myNumber1 = 3.14;
+        float myNumber2 = (float) myNumber1;
+        System.out.println(myNumber2);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+//        String class;
+
     }
 }
