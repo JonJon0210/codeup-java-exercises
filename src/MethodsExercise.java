b5ae5fb493769e01173c0ba4a80fe229a4dc877d
Jon Cortez <cortez.m.jon@gmail.com>
2021-01-26 15:00:41 -0600

    exercise number 2

--- src/MethodsExercise.java
+++ src/MethodsExercise.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
+
+
 public class MethodsExercise {
+
     public static int addMe(int value1, int value2) {
         return value1 + value2;
     }
@@ -15,11 +19,25 @@ public class MethodsExercise {
         return value1 / value2;
     }
 
-//    public static int modulusMe(int value1, int value2) {
+    //    public static int modulusMe(int value1, int value2) {
 //        return (value1 % 0, value2 % 0);
 //    }
 
+    public static int getInteger(int min, int max) {
+        Scanner sc = new Scanner(System.in);
+        int userInput = sc.nextInt();
+        if (userInput >= min && userInput <= max) {
+            System.out.println("You're age is " + userInput);
+        } else {
+            System.out.println("Please enter a valid number again.");
+            getInteger(min, max);
+        }
+        return 1;
+    }
+
+
     public static void main(String[] args) {
+
         int sum = MethodsExercise.addMe(1, 2);
         System.out.println(sum);
 
@@ -31,5 +49,10 @@ public class MethodsExercise {
 
         int quotient = MethodsExercise.divideMe(4, 2);
         System.out.println(quotient);
+
+        System.out.println("Enter a number between 1 and 10: ");
+        int userInput = getInteger(1, 10);
+
+
     }
 }
