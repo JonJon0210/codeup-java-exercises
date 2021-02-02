23e8177583b692d29dd7411cdf2549f0518df26a
Jon Cortez <cortez.m.jon@gmail.com>
2021-02-02 09:39:51 -0600

    updated Person class

--- src/Person.java
+++ src/Person.java
@@ -1,44 +1,34 @@
 class Person {
     private String name;
 
-    public Person (String name) {
+    Person(String name){
         this.name = name;
     }
 
-    public String getName() {
+    public String getName(){
+
         return this.name;
     }
-
-    public void setName(String name) {
+    public void setName(String name){
         this.name = name;
     }
-
-    public void sayHello() {
-        System.out.println("Hello, " + name);
+    public void sayHello(){
+        System.out.println("Hello, my name is " + this.name);
     }
 
-    public static void main(String[] args) {
-        Person bob = new Person ("Bob");
+    public static void main(String[] args){
+        Person bob = new Person("Bob");
         System.out.println(bob.getName());
         bob.setName("Bobby");
         bob.sayHello();
 
-//        Person person1 = new Person("John");
-//        Person person2 = new Person("John");
-//        System.out.println(person1.getName().equals(person2.getName()));
-//        System.out.println(person1 == person2);
-//
-//        Person person1 = new Person("John");
-//        Person person2 = person1;
-//        System.out.println(person1 == person2);
-
-
-//        Person person1 = new Person("John");
-//        Person person2 = person1;
-//        System.out.println(person1.getName());
-//        System.out.println(person2.getName());
-//        person2.setName("Jane");
-//        System.out.println(person1.getName());
-//        System.out.println(person2.getName());
+        Person person1 = new Person("John");
+        Person person2 = person1;
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
+        person2.setName("Jane");
+        System.out.println(person1.getName());
+        System.out.println(person2.getName());
     }
+
 }
