6cfe69b39ba2464e607ea9f5827db9263df38a71
Jon Cortez <cortez.m.jon@gmail.com>
2021-02-02 09:41:34 -0600

    updated Input class

--- src/util/Input.java
+++ src/util/Input.java
@@ -3,53 +3,72 @@ package util;
 import java.util.Scanner;
 
 public class Input {
+
     private Scanner scanner;
 
-    public Input () {
+    public Input(){
         this.scanner = new Scanner(System.in);
     }
 
-     public String getString() {
-        System.out.printf("Please enter a string");
+    public String getString(){
+        return getString("Please enter a string:");
+    }
+    public String getString(String prompt){
+        System.out.println(prompt);
         return this.scanner.nextLine();
     }
 
     public boolean yesNo() {
+        return yesNo("Please enter \"yes\" or \"no\": ");
+    }
+    public boolean yesNo(String prompt){
+        System.out.println(prompt);
+
         String answer = this.scanner.nextLine();
-        return (answer.toLowerCase().equals(equals("y"))) ||
-                answer.toLowerCase().equals("yes");
+        return (answer.toLowerCase().equals("y") ||
+                answer.toLowerCase().equals("yes"));
     }
 
-    public int getInt() {
-
+    public int getInt(){
+        return getInt("Please enter an integer: ");
+    }
+    public int getInt(String prompt){
+        System.out.println(prompt);
+        return this.scanner.nextInt();
     }
 
-    int getInt(int min, int max) {
+    public int getInt(int min, int max){
+        return getInt("Please enter an integer between "+min+" and "+max+": ",min,max);
+    }
+    public int getInt(String prompt, int min, int max){
         while (true) {
-            System.out.printf("Please enter an integer between %d and %d: ", min, max);
+            System.out.println(prompt);
             int answer = this.scanner.nextInt();
-            if (answer >= min && answer <= min) {
+            if (answer >= min && answer <= max)
                 return answer;
-            }
         }
     }
 
-    public double getDouble() {
+    public double getDouble(){
+        return getDouble("Please enter a double: ");
+    }
+    public double getDouble(String prompt){
+        System.out.println(prompt);
         return this.scanner.nextDouble();
     }
 
-    public double getDouble(double min, double max) {
+    public double getDouble(double min, double max){
+        return getDouble("Please enter a number between "+min+" and "+max+": ",min,max);
+    }
+    public double getDouble(String prompt, double min, double max){
         while (true) {
+            System.out.println(prompt);
             double answer = this.scanner.nextDouble();
-            if (answer >= min && answer <= min) {
+            if (answer >= min && answer <= max)
                 return answer;
-            }
         }
     }
-
-    public double getDouble() {
-        System.out.println("Enter a decimal number:");
-        return Double.parseDouble(scanner.nextLine());
-    }
+//    public static void main(String[] args) {
+//        Input in = new Input();
+//    }
 }
-
